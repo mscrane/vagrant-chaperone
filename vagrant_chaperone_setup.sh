dec57bfa5ccbb4494a77cde906b5333c5d8c2feb
Jake Dupuy <jake.dupuy@gmail.com>
2017-02-26 00:01:18 -0700

    fail scipt

--- vagrant_chaperone_setup.sh
+++ vagrant_chaperone_setup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+fail_script() {
+	local fail_message="$1"
+	local fail_code="$2"
+
+	echo -n "ERROR: "
+	if [ -z "$fail_message" ]; then
+		echo "Generic script failure."
+	else
+		echo "$fail_message"
+	fi
+
+	if [ -z "$fail_code" ]; then
+		fail_code=1
+	fi
+
+	exit $fail_code
+}
 ### Set CHAP_* ENV variables
 #
 set_chap_env() {
@@ -11,4 +28,9 @@ set_chap_env() {
   export CHAP_HOST_PROJECT_PORT=8811
 }
 
-set_chap_env
+vagrant_up() {
+  vagrant up
+}
+
+
+set_chap_env || fail_script "Failed to set CHAP ENV" 1
