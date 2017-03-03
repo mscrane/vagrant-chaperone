c6f15e958d729da9e72023bc4ea87ca6138fed9e
Jake Dupuy <jake.dupuy@gmail.com>
2017-03-03 15:27:33 -0700

    initial setup

--- vagrant_chaperone_setup.sh
+++ vagrant_chaperone_setup.sh
@@ -10,7 +10,7 @@ fail_script() {
   else
     echo "$fail_message"
   fi
- 
+
   if [ -z "$fail_code" ]; then
     fail_code=1
   fi
@@ -18,12 +18,19 @@ fail_script() {
 }
 
 set_chap_env() {
-  export CHAP_USE_PROXY='false'
-  export CHAP_LOCAL_REPO="/Users/jdupuy/bin/projects/05"
-  export CHAP_GUEST_IP="192.168.110.10"
-  export CHAP_VM_NAME="chaperone.dev"
+
+  local project_directory=${1}
+  local chaperone_ip_address=${2}
+
+  export CHAP_LOCAL_REPO=${project_directory}
+  export CHAP_GUEST_IP=${chaperone_ip_address}
+  export CHAP_VM_NAME="chaperone-ui.corp.local"
   export CHAP_HOST_ADMIN_PORT=8181
   export CHAP_HOST_PROJECT_PORT=8811
+
 }
 
-set_chap_env || fail_script "Failed to set CHAP ENV" 1
+project_dir=${1}
+chaperone_ip_address=${2}
+
+set_chap_env ${project_dir} ${chaperone_ip_address} || fail_script "Failed to set CHAP ENV" 1
